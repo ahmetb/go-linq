faa27c2226fe54d0246ee8ebe8e05b0d62eabb4e
zjhe <hezijie@microsoft.com>
2022-08-07 07:55:08 +0800

    Add IntersectG method

--- intersect.go
+++ intersect.go
@@ -29,6 +29,35 @@ func (q Query) Intersect(q2 Query) Query {
 	}
 }
 
+// Intersect produces the set intersection of the source collection and the
+// provided input collection. The intersection of two sets A and B is defined as
+// the set that contains all the elements of A that also appear in B, but no
+// other elements.
+func (q QueryG[T]) Intersect(q2 QueryG[T]) QueryG[T] {
+	return QueryG[T]{
+		Iterate: func() IteratorG[T] {
+			next := q.Iterate()
+			next2 := q2.Iterate()
+
+			set := make(map[interface{}]bool)
+			for item, ok := next2(); ok; item, ok = next2() {
+				set[item] = true
+			}
+
+			return func() (item T, ok bool) {
+				for item, ok = next(); ok; item, ok = next() {
+					if _, has := set[item]; has {
+						delete(set, item)
+						return
+					}
+				}
+
+				return
+			}
+		},
+	}
+}
+
 // IntersectBy produces the set intersection of the source collection and the
 // provided input collection. The intersection of two sets A and B is defined as
 // the set that contains all the elements of A that also appear in B, but no
@@ -50,6 +79,55 @@ func (q Query) IntersectBy(q2 Query,
 			}
 
 			return func() (item interface{}, ok bool) {
+				for item, ok = next(); ok; item, ok = next() {
+					s := selector(item)
+					if _, has := set[s]; has {
+						delete(set, s)
+						return
+					}
+				}
+
+				return
+			}
+		},
+	}
+}
+
+type Intersect[T any] interface {
+	Map(q1, q2 QueryG[T]) any
+}
+
+type intersectBy[TIn, TOut any] struct {
+	selector func(TIn) TOut
+}
+
+func IntersectSelector[TIn, TOut any](selector func(TIn) TOut) Intersect[TIn] {
+	return intersectBy[TIn, TOut]{
+		selector: selector,
+	}
+}
+
+func (i intersectBy[TIn, TOut]) Map(q1, q2 QueryG[TIn]) any {
+	return IntersectBy[TIn, TOut](q1, q2, i.selector)
+}
+
+func (q QueryG[T]) IntersectBy(q2 QueryG[T], i Intersect[T]) QueryG[T] {
+	return i.Map(q, q2).(QueryG[T])
+}
+
+func IntersectBy[T1, T2 any](q1, q2 QueryG[T1], selector func(T1) T2) QueryG[T1] {
+	return QueryG[T1]{
+		Iterate: func() IteratorG[T1] {
+			next := q1.Iterate()
+			next2 := q2.Iterate()
+
+			set := make(map[interface{}]bool)
+			for item, ok := next2(); ok; item, ok = next2() {
+				s := selector(item)
+				set[s] = true
+			}
+
+			return func() (item T1, ok bool) {
 				for item, ok = next(); ok; item, ok = next() {
 					s := selector(item)
 					if _, has := set[s]; has {
