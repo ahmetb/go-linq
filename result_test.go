10256e388164276272cd964349f920b10b2321bf
zjhe <hezijie@microsoft.com>
2022-08-06 23:34:03 +0800

    Add test case to ToChannelG method

--- result_test.go
+++ result_test.go
@@ -662,6 +662,7 @@ func TestSumG(t *testing.T) {
 	assert.Equal(t, int8(9), FromSliceG([]int8{int8(4), int8(5)}).Sum())
 	assert.Equal(t, 1, FromSliceG([]int{1}).Sum())
 	assert.Equal(t, 0, FromSliceG([]int{}).Sum())
+	assert.Equal(t, float64(9.), FromSliceG([]float64{1., 2., 2., 3., 1.}).Sum())
 }
 
 func TestSumInts(t *testing.T) {
