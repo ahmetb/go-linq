2eeb11399e98283eb990e7efbdaa509663ec3a78
zjhe <hezijie@microsoft.com>
2022-08-07 16:07:32 +0800

    Add Channel2ChannelG test

--- general_test.go
+++ general_test.go
@@ -1,6 +1,7 @@
 package linq
 
 import (
+	"github.com/stretchr/testify/assert"
 	"reflect"
 	"testing"
 )
@@ -33,4 +34,32 @@ func TestChannelToChannel(t *testing.T) {
 	if !reflect.DeepEqual(result, input) {
 		t.Errorf("FromChannel().ToChannel()=%v expected %v", result, input)
 	}
+}
+
+func TestChannelToChannelG(t *testing.T) {
+	input := []int{30, 40, 50}
+
+	inpCh := make(chan int)
+	resCh := make(chan int)
+
+	go func() {
+		for _, i := range input {
+			inpCh <- i
+		}
+
+		close(inpCh)
+	}()
+
+	go func() {
+		FromChannelG(inpCh).Where(func(i int) bool {
+			return i > 20
+		}).ToChannel(resCh)
+	}()
+
+	result := []int{}
+	for value := range resCh {
+		result = append(result, value)
+	}
+
+	assert.Equal(t, input, result)
 }
