289fae24046d5bb800f3958ee9a166ee322a6f93
zjhe <hezijie@microsoft.com>
2022-08-07 14:17:46 +0800

    Add TakeG methods

--- take.go
+++ take.go
@@ -20,6 +20,26 @@ func (q Query) Take(count int) Query {
 	}
 }
 
+// Take returns a specified number of contiguous elements from the start of a
+// collection.
+func (q QueryG[T]) Take(count int) QueryG[T] {
+	return QueryG[T]{
+		Iterate: func() IteratorG[T] {
+			next := q.Iterate()
+			n := count
+
+			return func() (item T, ok bool) {
+				if n <= 0 {
+					return
+				}
+
+				n--
+				return next()
+			}
+		},
+	}
+}
+
 // TakeWhile returns elements from a collection as long as a specified condition
 // is true, and then skips the remaining elements.
 func (q Query) TakeWhile(predicate func(interface{}) bool) Query {
@@ -45,6 +65,36 @@ func (q Query) TakeWhile(predicate func(interface{}) bool) Query {
 
 				done = true
 				return nil, false
+			}
+		},
+	}
+}
+
+// TakeWhile returns elements from a collection as long as a specified condition
+// is true, and then skips the remaining elements.
+func (q QueryG[T]) TakeWhile(predicate func(T) bool) QueryG[T] {
+	return QueryG[T]{
+		Iterate: func() IteratorG[T] {
+			next := q.Iterate()
+			done := false
+
+			return func() (item T, ok bool) {
+				if done {
+					return
+				}
+
+				item, ok = next()
+				if !ok {
+					done = true
+					return
+				}
+
+				if predicate(item) {
+					return
+				}
+
+				done = true
+				return *new(T), false
 			}
 		},
 	}
@@ -102,6 +152,41 @@ func (q Query) TakeWhileIndexed(predicate func(int, interface{}) bool) Query {
 
 				done = true
 				return nil, false
+			}
+		},
+	}
+}
+
+// TakeWhileIndexed returns elements from a collection as long as a specified
+// condition is true. The element's index is used in the logic of the predicate
+// function. The first argument of predicate represents the zero-based index of
+// the element within collection. The second argument represents the element to
+// test.
+func (q QueryG[T]) TakeWhileIndexed(predicate func(int, T) bool) QueryG[T] {
+	return QueryG[T]{
+		Iterate: func() IteratorG[T] {
+			next := q.Iterate()
+			done := false
+			index := 0
+
+			return func() (item T, ok bool) {
+				if done {
+					return
+				}
+
+				item, ok = next()
+				if !ok {
+					done = true
+					return
+				}
+
+				if predicate(index, item) {
+					index++
+					return
+				}
+
+				done = true
+				return *new(T), false
 			}
 		},
 	}
