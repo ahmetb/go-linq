4943821fc2b0173c53063c0aaf11a703abf4b6ba
zjhe <hezijie@microsoft.com>
2022-08-07 16:50:13 +0800

    Add ReverseG method

--- reverse.go
+++ reverse.go
@@ -28,3 +28,27 @@ func (q Query) Reverse() Query {
 		},
 	}
 }
+
+func (q QueryG[T]) Reverse() QueryG[T] {
+	return QueryG[T]{
+		Iterate: func() IteratorG[T] {
+			next := q.Iterate()
+
+			items := []T{}
+			for item, ok := next(); ok; item, ok = next() {
+				items = append(items, item)
+			}
+
+			index := len(items) - 1
+			return func() (item T, ok bool) {
+				if index < 0 {
+					return
+				}
+
+				item, ok = items[index], true
+				index--
+				return
+			}
+		},
+	}
+}
